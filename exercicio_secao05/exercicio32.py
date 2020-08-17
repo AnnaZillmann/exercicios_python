5e9a4806726cef582e31ffb48b94173bf86d14d9
Anna Zillmann <annazillmannbastos@gmail.com>
2020-08-17 14:23:50 -0300

    Exercício 32 da seção 05 do Geek University

---
+++ exercicio_secao05/exercicio32.py
@@ -0,0 +1,23 @@
+produto = float(input('Informe o cóodigo do produto.\n'
+                      '100. Cachorro quente R$ 1.20\n'
+                      '101. Bauru Simples R$ 1.30\n'
+                      '102. Bauru com ovo R$ 1.50\n'
+                      '103. Hamburger R4 1.20\n'
+                      '104. Cheeseburguer R$ 1.70\n'
+                      '105. Suco R$ 2.20\n'
+                      '106. Refrigernte R$ 1.00\n'
+                      'Opção:'))
+if produto == 100:
+    print('Valor a ser pago R$ 1.20')
+elif produto == 101:
+    print('Valor a ser pago R$ 1.30')
+elif produto == 102:
+    print('Valor a ser pago R$ 1.50')
+elif produto == 103:
+    print('Valor a ser pago R$ 1.20')
+elif produto == 104:
+    print('Valor a ser pago R$ 1.70')
+elif produto == 105:
+    print('Valor a ser pago R$ 2.20')
+elif produto == 106:
+    print('Valor a ser pago R$ 1.00')
